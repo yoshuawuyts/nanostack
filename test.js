83f2e002b20f64cd1d5efe28c72c66d6732744ea
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-02-25 17:37:58 +0100

    more tests

--- test.js
+++ test.js
@@ -43,7 +43,7 @@ tape('nanobus.use()', function (t) {
   })
 
   t.test('should allow chaining middleware', function (t) {
-    t.plan(10)
+    t.plan(13)
     var order = 0
 
     var bus = nanobus()
@@ -53,7 +53,7 @@ tape('nanobus.use()', function (t) {
       t.equal(order++, 1, 'order is 1')
       next(null, function (err, val, next) {
         t.ifError(err, '.use(): next no err')
-        t.equal(order++, 5, 'order is 5')
+        t.equal(order++, 7, 'order is 7')
         next()
       })
     })
@@ -62,13 +62,22 @@ tape('nanobus.use()', function (t) {
       t.equal(order++, 2, 'order is 2')
       next(null, function (err, val, next) {
         t.ifError(err, '.use(): next no err')
-        t.equal(order++, 4, 'order is 4')
+        t.equal(order++, 6, 'order is 6')
         next()
       })
     })
 
     bus.use(function (ctx, next) {
       t.equal(order++, 3, 'order is 3')
+      next(null, function (err, val, next) {
+        t.ifError(err, '.use(): next no err')
+        t.equal(order++, 5, 'order is 5')
+        next()
+      })
+    })
+
+    bus.use(function (ctx, next) {
+      t.equal(order++, 4, 'order is 4')
       next()
     })
 
@@ -76,10 +85,86 @@ tape('nanobus.use()', function (t) {
       t.equal(order++, 0, 'order is 0')
       next(ctx, function (err, data) {
         t.ifError(err, '.from(): next no err')
-        t.equal(order++, 6, 'order is 6')
+        t.equal(order++, 8, 'order is 8')
       })
     })
   })
-  t.test('should allow aborting middleware')
-  t.test('should handle errors')
+
+  t.test('should allow aborting middleware', function (t) {
+    t.plan(4)
+    var bus = nanobus()
+    var ctx = {}
+
+    bus.use(function (ctx, next) {
+      t.pass('.use() was called')
+      next(null, 'hi')
+    })
+
+    bus.use(function (ctx, next) {
+      t.fail('should not be called')
+      next()
+    })
+
+    bus.from(function (next) {
+      next(ctx, function (err, data) {
+        t.ifError(err, 'no error')
+        t.equal(data, 'hi')
+        t.pass('resolved')
+      })
+    })
+  })
+
+  t.test('should handle errors', function (t) {
+    t.plan(2)
+    var bus = nanobus()
+    var ctx = {}
+
+    bus.use(function (ctx, next) {
+      next(null, function (err, val, next) {
+        t.ok(err, 'err found')
+        next(err)
+      })
+    })
+
+    bus.use(function (ctx, next) {
+      var err = new Error('mate')
+      next(err)
+    })
+
+    bus.from(function (next) {
+      next(ctx, function (err, data) {
+        t.ok(err, 'err found')
+      })
+    })
+  })
+
+  t.test('should have a shared ctx', function (t) {
+    t.plan(7)
+    var bus = nanobus()
+    var ctx = {}
+
+    bus.use(function (ctx, next) {
+      t.equal(ctx.foo, 'bar')
+      next(null, function (err, val, next) {
+        t.ifError(err, 'no err found')
+        t.equal(ctx.beep, 'boop')
+        next()
+      })
+    })
+
+    bus.use(function (ctx, next) {
+      t.equal(ctx.foo, 'bar')
+      ctx.beep = 'boop'
+      next()
+    })
+
+    bus.from(function (next) {
+      ctx.foo = 'bar'
+      next(ctx, function (err, data) {
+        t.ifError(err, 'no err found')
+        t.equal(ctx.foo, 'bar', 'bar found')
+        t.equal(ctx.beep, 'boop', 'boop found')
+      })
+    })
+  })
 })
